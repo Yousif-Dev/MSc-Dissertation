9e566d15e895090b00d10cf01d946c23f077364f
Cocohop <48159236+Cocohop@users.noreply.github.com>
2020-04-24 15:09:09 +0100

    Removed efficiency function
    Don't think it's needed

--- Main.py
+++ Main.py
@@ -9,14 +9,17 @@ from pandas.plotting import register_matplotlib_converters
 
 class SolarCell(object):
 
-    def __init__(self, solar_cell_location, inverter_location, maximum_power, loc = [52.4068,1.5197]):
+    def __init__(self, solar_cell_location, inverter_location, maximum_power, loc=[52.4068, 1.5197], tilt=0,
+                 azimuth=180):
         self.solar_cell = pd.read_csv(solar_cell_location, index_col=0, parse_dates=True)
         self.inverter = pd.read_csv(inverter_location)
         self.max_power = maximum_power
         self.inverter["Percent"] = self.inverter["Percent"] * self.max_power / 100
         self.inverter.rename(columns={"Percent": "Power"})
-        self.erbs = 0
         self.loc = loc
+        self.tilt = tilt
+        self.azimuth = azimuth
+        self.panel = 0
     def return_cell(self):
         return self.solar_cell
 
@@ -30,17 +33,10 @@ class SolarCell(object):
             self.solar_cell.at[dateandtimeValue, "GHI"] = self.solar_cell.at[dateandtimeValue, "GHI"] * \
                                                           self.inverter.at[
                                                               result_index, "Efficiency"] / 100
-    def efficiency(self):
-        hourly_resampled = self.solar_cell.resample("H").mean()
-        date = pd.date_range(start = hourly_resampled.index[0], end = hourly_resampled.index[-1], freq = "H")
-        print(date)
-        solar_pos = pvlib.solarposition.get_solarposition(date,self.loc[0],self.loc[1])
-        self.erbs = pvlib.irradiance.erbs(hourly_resampled["GHI"], solar_pos["apparent_zenith"], date)
-        return(self.erbs)
+
+
 
 if __name__ == "__main__":
-
     solar_cell = SolarCell(
         r"C:\Users\Coco\Desktop\Personal Folder\Study\-MSc\Dissertation\Data\ss_testbed_irrad_2012.csv",
         r"C:\Users\Coco\Desktop\Personal Folder\Study\-MSc\Dissertation\Data\Inverter.csv", 10000)
-    print(solar_cell.efficiency())
